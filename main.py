a5f65b0df7d1357979dcd28be45cfd175517e6be
AlsuV <165937069+AlsuV@users.noreply.github.com>
2024-08-04 21:57:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def get_matrix(n, m, value):
+    print()
+    get_matrix()
+    matrix = []
+    for n in matrix:
+        n == 0
+        n.append(get_matrix)
+        for m in matrix:
+            m == 0
+            m.append(get_matrix)
+            value = 1
+        result1 = get_matrix(2, 2, 10)
+        result2 = get_matrix(3, 5, 42)
+        result3 = get_matrix(4, 2, 13)
+        print(result1)
+        print(result2)
+        print(result3)
